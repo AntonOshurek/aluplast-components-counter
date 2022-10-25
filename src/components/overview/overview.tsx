e306585bdb2e4233371c7820985007b233fac785
Anton Oshurek <oshurekweb@gmail.com>
2022-10-25 21:08:15 +0200

    add basic overview

--- src/components/overview/overview.tsx
+++ src/components/overview/overview.tsx
@@ -1,8 +1,48 @@
+import './overview.scss';
+
+
 const Overview = (): JSX.Element => {
   return (
-    <main>
+    <section className="overview container">
+      <div className='overview__header'>
+        <h2 className='overview__title'>Overview</h2>
+        <span className='overview__date'>25.10.2022</span>
+      </div>
+      <ul className='granulates-list'>
+        <li className='granulates-list__item'>
+          <div className='granulates-list__image-wrap'>
+            <img className='granulates-list__image' src="./images/granulat-img.jpg" width='100' height='100' alt="" />
+          </div>
+          <div className='granulates-list__info-block'>
+            <h3 className='granulates-list__granulate-name'>Granulates name</h3>
+            <p className='granulates-list__granulate-description'>granulates description information</p>
+          </div>
+          <span className='granulates-list__granulates-amount'>250 kg</span>
+        </li>
 
-    </main>
+        <li className='granulates-list__item'>
+          <div className='granulates-list__image-wrap'>
+            <img className='granulates-list__image' src="./images/granulat-img.jpg" width='100' height='100' alt="" />
+          </div>
+          <div className='granulates-list__info-block'>
+            <h3 className='granulates-list__granulate-name'>Granulates name</h3>
+            <p className='granulates-list__granulate-description'>granulates description information</p>
+          </div>
+          <span className='granulates-list__granulates-amount'>250 kg</span>
+        </li>
+
+        <li className='granulates-list__item'>
+          <div className='granulates-list__image-wrap'>
+            <img className='granulates-list__image' src="./images/granulat-img.jpg" width='100' height='100' alt="" />
+          </div>
+          <div className='granulates-list__info-block'>
+            <h3 className='granulates-list__granulate-name'>Granulates name</h3>
+            <p className='granulates-list__granulate-description'>granulates description information</p>
+          </div>
+          <span className='granulates-list__granulates-amount'>250 kg</span>
+        </li>
+      </ul>
+    </section>
   )
 }
 
