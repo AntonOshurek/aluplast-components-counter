2875949a11aa89377b780e4bb80ef3bebc2de697
Anton Oshurek <oshurekweb@gmail.com>
2022-10-26 15:40:20 +0200

    add granulates basic data and types

---
+++ src/data/granulates-data.ts
@@ -0,0 +1,19 @@
+import { GranulateDataTypes } from "../types/granulate-data-types";
+
+export const granulatesData: GranulateDataTypes = [
+  {
+    name: 'ochra',
+  },
+  {
+    name: 'antracyt',
+  },
+  {
+    name: 'crem',
+  },
+  {
+    name: 'brąnz',
+  },
+  {
+    name: 'braun',
+  },
+];
---
+++ src/types/granulate-data-types.ts
@@ -0,0 +1,6 @@
+export type GranulateItemType = {
+  name: string,
+
+}
+
+export type GranulateDataTypes = GranulateItemType[]
