43f932dee27d89855b182c3057ab329a33ebad99
Anton Oshurek <oshurekweb@gmail.com>
2022-10-28 20:04:05 +0200

    add counter controls buttons

--- src/components/granulates-counter/granulates-counter/granulates-counter.tsx
+++ src/components/granulates-counter/granulates-counter/granulates-counter.tsx
@@ -4,7 +4,16 @@ const GranulatesCounter = (): JSX.Element => {
 
   return (
     <section className="granulates-counter">
-      <h3>GranulatesCounter</h3>
+      <h3 className='visually-hidden'>GranulatesCounter</h3>
+
+      <div className='granulates-counter__counter-value'>
+        <span>0</span>
+      </div>
+
+      <div className='granulates-counter__controls'>
+        <button className='granulates-counter__button' type='button'>Minus</button>
+        <button className='granulates-counter__button granulates-counter__button--main' type='button'>Plus</button>
+      </div>
     </section>
   )
 }
