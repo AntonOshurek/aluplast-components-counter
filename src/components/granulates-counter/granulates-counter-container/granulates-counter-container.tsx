ea6eabf78c074d8c8d92d293e6cd2af80281a730
Anton Oshurek <oshurekweb@gmail.com>
2022-10-31 12:57:04 +0100

    add container counter

--- src/components/granulates-counter/granulates-counter-container/granulates-counter-container.tsx
+++ src/components/granulates-counter/granulates-counter-container/granulates-counter-container.tsx
@@ -1,7 +1,29 @@
+import './granulates-counter-container.scss';
+
 const GranulatesCounterContainer = (): JSX.Element => {
   return (
-    <section>
-      GranulatesCounterContainer
+    <section className="granulates-counter__container">
+      <h3 className='visually-hidden'>GranulatesCounter</h3>
+
+      <div className='counter-container__set-contols'>
+        <label className='counter-container__set-val-label'>
+          adding value -
+          <input className='counter-container__set-val-input' defaultValue='75' type="number" />
+          kg
+        </label>
+      </div>
+
+      <div className='counter-container__input-wrap'>
+        <label className='counter-container__number-label'>
+          Add
+          <input className='counter-container__number-input' defaultValue='500' type="number" />
+          kilos
+        </label>
+      </div>
+
+      <div className='counter-container__controls'>
+        <button className='counter-container__button counter-container__button--main' type='button'>Add value</button>
+      </div>
     </section>
   )
 }
