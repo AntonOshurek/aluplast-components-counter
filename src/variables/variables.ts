446ae4fe196ced4061abc0c1ecaa86604834b3e2
Anton Oshurek <oshurekweb@gmail.com>
2022-11-02 22:27:41 +0100

    routes change

--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -1,7 +1,7 @@
 export const enum AppRoute {
-  ROOT = '/aluplas-granules-counter/',
+  ROOT = '/aluplas-granules-counter',
   GRANULATES_PAGE = '/granulates',
-  GRANULATES_COUNTER = '/granulates/:UNID/',
+  GRANULATES_COUNTER = '/granulates/:UNID',
   GRANULATES_COUNTER_CONTAINER = 'container',
   GRANULATES_COUNTER_VOREK = 'vorek',
 }
