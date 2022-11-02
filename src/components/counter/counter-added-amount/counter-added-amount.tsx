85414477f8f66a8e8e995f7fdb0abc8e6840648c
Anton Oshurek <oshurekweb@gmail.com>
2022-11-02 22:19:29 +0100

    add blur logick for inputs

--- src/components/counter/counter-added-amount/counter-added-amount.tsx
+++ src/components/counter/counter-added-amount/counter-added-amount.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent} from 'react'
+import { ChangeEvent, useRef } from 'react'
 
 import './counter-added-amount.scss';
 
@@ -9,13 +9,27 @@ interface CounterAddedAmountPropsType {
 }
 
 const CounterAddedAmount = ({onInputChangeHandler, value, title}: CounterAddedAmountPropsType): JSX.Element => {
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const onInputKeydownListener = (evt: any) => {
+    if(evt.keyCode === 13 || evt.keyCode === 'enter') {
+      evt.preventDefault();
+
+      if(inputEl.current !== null) {
+        inputEl.current.blur();
+      }
+    }
+  }
+
   return (
     <div className='counter-added-amount'>
       <label className='counter-added-amount__label'>
         {title}
         <input className='counter-added-amount__input' type="number"
+          ref={inputEl}
           value={value ? value : ''}
           onChange={onInputChangeHandler}
+          onKeyDown={onInputKeydownListener}
         />
       </label>
     </div>
--- src/components/counter/counter-set-value/counter-set-value.tsx
+++ src/components/counter/counter-set-value/counter-set-value.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useRef } from 'react';
 
 import './counter-set-value.scss';
 
@@ -8,14 +8,27 @@ interface CouterSetValueProps {
 }
 
 const CounterSetValue = ({onInputChangeHandler, value}: CouterSetValueProps): JSX.Element => {
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const onInputKeydownListener = (evt: any) => {
+    if(evt.keyCode === 13 || evt.keyCode === 'enter') {
+      evt.preventDefault();
+
+      if(inputEl.current !== null) {
+        inputEl.current.blur();
+      }
+    }
+  }
 
   return (
     <div className='counter-set-value'>
       <label className='counter-set-value__label'>
         Add
         <input className='counter-set-value__input' type="number"
+          ref={inputEl}
           value={value ? value : ''}
           onChange={onInputChangeHandler}
+          onKeyDown={onInputKeydownListener}
         />
         kilos
       </label>
