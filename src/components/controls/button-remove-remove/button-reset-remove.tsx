6f174f7abfad5bad95687f321a4b433b0950de7e
Anton Oshurek <oshurekweb@gmail.com>
2022-11-18 12:59:36 +0100

    add new styles for accept modal

--- src/components/controls/button-remove-remove/button-reset-remove.tsx
+++ src/components/controls/button-remove-remove/button-reset-remove.tsx
@@ -1,8 +1,4 @@
 import { useState } from 'react';
-//components
-import ButtonChoice from '../button-choice/button-choice';
-//variables and consts
-import { ComponentsTexts } from '../../../variables/variables';
 //styles
 import './button-reset-remove.scss';
 
@@ -40,12 +36,26 @@ const ButtonResetRemove = ({onButtonClickHandler, buttonText}: ButtonResetRemove
 
       {
         showModal ? <div className='button-reset-remove__accept'>
-          <h2 className='button-reset-remove__accept-title'>Napewno chcesz {buttonText}?</h2>
-          <ButtonChoice
-            onIncButtonClickHandler={confirmButtonClickHandler}
-            onDecButtonClickHandler={onShowAcceptModalClickHandler}
-            choiseText={{plus: ComponentsTexts.CONFIRM, minus: ComponentsTexts.CANCEL}}/>
-        </div> : null
+            <div className='button-reset-remove__accept-wrap'>
+              <h2 className='button-reset-remove__accept-title'>Napewno chcesz {buttonText}?</h2>
+              <div className='button-reset-remove__accept-controls'>
+                <button className='button-reset-remove__accept-button button-reset-remove__accept-button--cancel'
+                  type='button'
+                  onClick={closeSuccessModal}>
+                  Odwołaj
+                </button>
+                <button className='button-reset-remove__accept-button button-reset-remove__accept-button--confirm'
+                  type='button'
+                  onClick={confirmButtonClickHandler}>
+                  Usunąć wszystkie dane
+                </button>
+              </div>
+              <figure className='button-reset-remove__accept-icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40"><path fill='#ffffff' d="M10.75 36.208q-1.875 0-3.167-1.27-1.291-1.271-1.291-3.146V9.5h-2.5V5.083h10.083V2.875h12.208v2.208h10.084V9.5h-2.5v22.292q0 1.833-1.292 3.125-1.292 1.291-3.125 1.291ZM29.25 9.5h-18.5v22.292h18.5ZM14.542 28.667h3.625V12.583h-3.625Zm7.333 0H25.5V12.583h-3.625ZM10.75 9.5v22.292Z"/></svg>
+              </figure>
+            </div>
+          </div>
+        : null
       }
 
       {
